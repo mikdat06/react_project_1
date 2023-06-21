b861dbfe2dddae5d53443c6efb3f631bc859f247
Ali Ozkan <aliozkan4334@gmail.com>
2023-06-22 01:20:01 +0200

    need to add data.js for json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import Header from './Header';
-import Main from "./Main/Main"
+import Header from "./Header";
+import Main from "./Main/Main";
 
 function App() {
-  return( <>
- <Header/>
-  <Main/>
-  </>)
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
--- src/Main/React.js
+++ src/Main/React.js
@@ -1,22 +1,22 @@
-import img from "../img/React.png"
+import img from "../img/React_logo.png";
 
-const REACT=()=> {
+const REACT = () => {
   return (
-    <div class="lesson_lessoncol col-md-6 col-lg-4">
-    <div>
-      <img src={img} alt="REACT" />
+    <div class="lesson_lesson col col-md-6 col-lg-4">
+      <div>
+        <img src={img} alt="REACT" />
+      </div>
+      <div class="lesson_text">
+        <p class="lesson_parag">
+          <span class="lesson_span">Lesson Name :</span>
+          REACT
+        </p>
+        <p class="lesson_parag">
+          <span class="lesson_span">Lesson Hour :</span>
+          14:15 pm
+        </p>
+      </div>
     </div>
-    <div class="lesson_text">
-      <p class="lesson_parag">
-        <span class="lesson_span">Lesson Name :</span>
-        REACT
-      </p>
-      <p class="lesson_parag">
-        <span class="lesson_span">Lesson Hour :</span>
-        14:15 pm
-      </p>
-    </div>
-  </div>
-  )
-}
-export default REACT
+  );
+};
+export default REACT;
